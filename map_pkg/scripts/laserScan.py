95bdd4001b4aaaaa6c43cd264c71e2a360c38a62
Morgan Dykshorn <mdd27@vt.edu>
2015-11-25 16:45:44 -0500

    rewrote laserscan class. made the stepping function happen inside scanning function to reduce math and get smoother stepping

--- map_pkg/scripts/laserScan.py
+++ map_pkg/scripts/laserScan.py
@@ -1,66 +1,110 @@
 #!/usr/bin/python
+#stepper framework by petebachant
+#scanning and changes made by Morgan Dykshorn
 #imports necessary libraries
 import time
-from bbpystepper import Stepper
+import math
+from __future__ import division
 import Adafruit_BBIO.ADC as ADC
+import Adafruit_BBIO.GPIO as GPIO
+
+def initialize_pins(pins):
+	for pin in pins:
+		GPIO.setup(pin, GPIO.OUT)
+
+def set_all_pins_low(pins):
+	for pin in pins:
+		GPIO.output(pin, GPIO.LOW)
+		
+def wavedrive(pins, pin_index):
+	for i in range(len(pins)):
+		if i == pin_index:
+			GPIO.output(pins[i], GPIO.HIGH)
+		else:
+			GPIO.output(pins[i], GPIO.LOW)
+
+def fullstep(pins, pin_index):
+	"""pin_index is the lead pin"""
+	GPIO.output(pins[pin_index], GPIO.HIGH)
+	GPIO.output(pins[(pin_index+3) % 4], GPIO.HIGH)
+	GPIO.output(pins[(pin_index+1) % 4], GPIO.LOW)
+	GPIO.output(pins[(pin_index+2) % 4], GPIO.LOW)
 
 
-class laserScan:
-	def __init__(self):
+class laserScan(object):
+	def __init__(self, steps_per_rev=2048.0,
+				 pins=["P8_13", "P8_14", "P8_15", "P8_16"]):
+
+		self.pins = pins
+		
+		initialize_pins(self.pins)
+		set_all_pins_low(self.pins)
+		
+		self.angle = 0
+		self.steps_per_rev = steps_per_rev
+		
+		# Initialize stepping mode
+		self.drivemode = fullstep
+
 		#sets up ADC
 		ADC.setup()
-		#creates an instance of the stepper class
-		self.mystepper = Stepper()
 		#cretes a value for the threshold
 		self.threshold = .7
-		#creates a variable for the current degree measurement
-		self._degree = 0
-		self._distance = 0
-
+		#creates a variable for the distance measurment
+		self.distance = 0
 
 	def calibrate(self):
+		#sets angle to 1 so loop will run
+		self.angle = 1.0
 
-		#rotate once to change angle from 0
-		self.mystepper.rotate(3.6, 75)
+		while self.angle!=0:
 
-		while self.mystepper.angle!=0:
+			for pin_index in range(len(self.pins)):
+				self.drivemode(self.pins, pin_index)
+				#waits for .0025 seconds keeping the sensor reading at 100hz
+				time.sleep(.0025)
+				step += 1
+				#checks for when the sensor triggers 0 angle calibration
+				#reads value from sensor
+				rotateVal = ADC.read("P9_39")
+				if rotateVal < self.threshold:
+					self.angle = 0			
+		set_all_pins_low(self.pins)	
 
-			#rotates the steper 3.6 degrees or 4 steps(after gearing)
-			self.mystepper.rotate(3.6, 75)
-			#reads value from sensor
-			rotateVal = ADC.read("P9_39")
-			#checks if value is below the threshold
-			if rotateVal < self.threshold:
-				self.mystepper.zero_angle()
-			#sleeps for .01 seconds to not overload motor
-			time.sleep(.1)
+	
+	def scan(self):
+		
+		#runs indefinitely
+		while 1:
+			#set step to 0
+			step = 0
+			# sets ammount of steps for each time function is called
+			steps = 4
+			self.direction = 1
+		
+			while step < steps:
+				for pin_index in range(len(self.pins)):
+					self.drivemode(self.pins, pin_index)
+					#waits for .0025 seconds keeping the sensor reading at 100hz
+					time.sleep(.0025)
+					step += 1
+					#checks for when the sensor triggers 0 angle calibration
+					#reads value from sensor
+					rotateVal = ADC.read("P9_39")
+					if rotateVal < self.threshold:
+						self.angle = 0
+					else:
+						self.angle = self.angle + .9
 
 
-	#function that scans with inputs of speed(in rpm)
-	def scan(self, speed):
-		#checks for when the sensor is over the 0 angle calibration
-		#and if it is resets the angle
-		#reads value from sensor
-		rotateVal = ADC.read("P9_39")
-		#reads value from sensor
-		if rotateVal < self.threshold:
-			self.mystepper.zero_angle()
+			#read data from LIDAR (need to figure out how)
+				
+			#set the read distance to the distance variable
+			self.distance = 2.0
 
-		#read data from LIDAR (need to figure out how)
-
-		#set the degree measurment to the current calculated angle
-		self._degree = self.mystepper.angle
-		self._distance = 2 #temporarily assigns distance to 2m
-
-		#rotates the steper 3.6 degrees or 4 steps(after gearing)
-		self.mystepper.rotate(3.6, speed)
-
-	@property
-	def degree(self):
-		return self._degree
-	@property
-	def distance(self):
-		return self._distance	
+			print self.angle, self.distance
+	
+		set_all_pins_low(self.pins)	
 
 
 if __name__ == '__main__':
@@ -69,17 +113,5 @@ if __name__ == '__main__':
 	lScan = laserScan()
 
 	lScan.calibrate()
-	#sets the length of scan
-	scantime = input('how many datapoints')
-	count = 0
 
-	#runs while count is less than the number of datapoints
-	while count<scantime:
-		lScan.scan(75)
-		Distance = lScan.distance()
-		Degree = lScan.degree()
-
-		print Degree, Distance
-
-		#sleeps for .01 seconds, keeping the rate of scanning 100hz
-		time.sleep(.5)
+	lScan.scan()
