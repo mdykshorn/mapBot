b4959f4fc215d410d328c3bbab3cef6a89d612d5
Morgan Dykshorn <mdd27@vt.edu>
2015-11-25 17:27:50 -0500

    changed rate

--- map_pkg/scripts/laserScan.py
+++ map_pkg/scripts/laserScan.py
@@ -61,7 +61,7 @@ class laserScan(object):
 			for pin_index in range(len(self.pins)):
 				self.drivemode(self.pins, pin_index)
 				#waits for .0025 seconds keeping the sensor reading at 100hz
-				time.sleep(.05)
+				time.sleep(.0025)
 				#checks for when the sensor triggers 0 angle calibration
 				#reads value from sensor
 				rotateVal = ADC.read("P9_39")
@@ -84,7 +84,7 @@ class laserScan(object):
 				for pin_index in range(len(self.pins)):
 					self.drivemode(self.pins, pin_index)
 					#waits for .0025 seconds keeping the sensor reading at 100hz
-					time.sleep(.05)
+					time.sleep(.0025)
 					step += 1
 					#checks for when the sensor triggers 0 angle calibration
 					#reads value from sensor
