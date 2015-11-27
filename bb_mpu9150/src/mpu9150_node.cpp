231371e9adffb3333dd8ce428d7ce0dbcd9739ed
Morgan Dykshorn <mdd27@vt.edu>
2015-11-26 22:22:33 -0500

    deleted unnecesary functions

--- bb_mpu9150/src/mpu9150_node.cpp
+++ bb_mpu9150/src/mpu9150_node.cpp
@@ -44,31 +44,12 @@
 
 
 int set_cal(int mag, char *cal_file);
-void read_loop(unsigned int sample_rate);
 void register_sig_handler();
 void sigint_handler(int sig);
 
 int done;
 
-void usage(char *argv_0)
-{
-    printf("\nUsage: %s [options]\n", argv_0);
-    printf("  -b <i2c-bus>          The I2C bus number where the IMU is. The default is 1 to use /dev/i2c-1.\n");
-    printf("  -s <sample-rate>      The IMU sample rate in Hz. Range 2-50, default 10.\n");
-    printf("  -y <yaw-mix-factor>   Effect of mag yaw on fused yaw data.\n");
-    printf("                           0 = gyro only\n");
-    printf("                           1 = mag only\n");
-    printf("                           > 1 scaled mag adjustment of gyro data\n");
-    printf("                           The default is 4.\n");
-    printf("  -a <accelcal file>    Path to accelerometer calibration file. Default is ./accelcal.txt\n");
-    printf("  -m <magcal file>      Path to mag calibration file. Default is ./magcal.txt\n");
-    printf("  -v                    Verbose messages\n");
-    printf("  -h                    Show this help\n");
 
-    printf("\nExample: %s -b3 -s20 -y10\n\n", argv_0);
-
-    exit(1);
-}
 
 int main(int argc, char **argv)
 {
@@ -91,82 +72,6 @@ int main(int argc, char **argv)
 	unsigned long loop_delay;
 	mpudata_t mpu;
 
-    // receive the parameters and process them
-    while ((opt = getopt(argc, argv, "b:s:y:a:m:vh")) != -1) {
-        switch (opt) {
-        case 'b':
-            i2c_bus = strtoul(optarg, NULL, 0);
-
-            if (errno == EINVAL)
-                usage(argv[0]);
-
-            if (i2c_bus < MIN_I2C_BUS || i2c_bus > MAX_I2C_BUS)
-                usage(argv[0]);
-
-            break;
-
-        case 's':
-            sample_rate = strtoul(optarg, NULL, 0);
-
-            if (errno == EINVAL){
-                printf("EINVAL\n");
-                usage(argv[0]);
-            }
-
-            if (sample_rate < MIN_SAMPLE_RATE || sample_rate > MAX_SAMPLE_RATE){
-                printf("sample rate problem\n");
-                usage(argv[0]);
-            }
-
-            break;
-
-        case 'y':
-            yaw_mix_factor = strtoul(optarg, NULL, 0);
-
-            if (errno == EINVAL)
-                usage(argv[0]);
-
-            if (yaw_mix_factor < 0 || yaw_mix_factor > 100)
-                usage(argv[0]);
-
-            break;
-
-        case 'a':
-            len = 1 + strlen(optarg);
-
-            accel_cal_file = (char *)malloc(len);
-
-            if (!accel_cal_file) {
-                perror("malloc");
-                exit(1);
-            }
-
-            strcpy(accel_cal_file, optarg);
-            break;
-
-        case 'm':
-            len = 1 + strlen(optarg);
-
-            mag_cal_file = (char *)malloc(len);
-
-            if (!mag_cal_file) {
-                perror("malloc");
-                exit(1);
-            }
-
-            strcpy(mag_cal_file, optarg);
-            break;
-
-        case 'v':
-            verbose = 1;
-            break;
-
-        case 'h':
-        default:
-            usage(argv[0]);
-            break;
-        }
-    }
 
     // Initialize the MPU-9150
 	register_sig_handler();
