d9cad731e07f717c7c6e122a343c06fa2a1b6486
Morgan <mdd27@vt.edu>
2018-01-14 10:37:28 -0500

    removing build and devel directories

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mdykshorn/mapBot/src
-# Build directory: /home/mdykshorn/mapBot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(general_khan)
--- build/general_khan/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mdykshorn/mapBot/src/general_khan
-# Build directory: /home/mdykshorn/mapBot/build/general_khan
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
